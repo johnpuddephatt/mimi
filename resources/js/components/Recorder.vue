687653cb082973fc76ea14b3a598a91f2bd3353b
John Puddephatt <john@jdp.org.uk>
2020-03-13 20:29:30 +0000

    show mimetype

--- resources/js/components/Recorder.vue
+++ resources/js/components/Recorder.vue
@@ -48,7 +48,7 @@
    </div>
 
    <div class="card" v-else-if="recordingData">
-      <h5 class="card-header">Review video</h5>
+      <h5 class="card-header">Review video <small class="float-right">{{recordingMimeType}}</small></h5>
       <div class="card-body has-square-media">
          <video-player :source="recordingUrl" :type="mimeType"></video-player>
       </div>
