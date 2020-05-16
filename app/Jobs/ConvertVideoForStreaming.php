fe01ce081e96c48e2ca7a37f343a77ca981540ee
John Puddephatt <john@jdp.org.uk>
2020-05-16 15:50:03 +0100

    reduce segment length

--- app/Jobs/ConvertVideoForStreaming.php
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -53,6 +53,7 @@ class ConvertVideoForStreaming implements ShouldQueue
             ->addFilter('-vf', "crop='min(iw,ih)':'min(iw,ih)',scale=480:480")
            // call the 'exportForHLS' method and specify the disk to which we want to export...
             ->exportForHLS()
+            ->setSegmentLength(5)
             ->toDisk($this->video->disk)
            // we'll add different formats so the stream will play smoothly
             ->addFormat($lowBitrateFormat)
