e12ec881bc68bd6ed6f2870cacf64298ab038adb
John Puddephatt <john@jdp.org.uk>
2020-12-16 15:59:51 +0000

    add debugbar with ?debug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+      if(isset($_GET['debug'])){
+        \Debugbar::enable();
+       }
     }
 
     /**
