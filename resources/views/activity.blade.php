43725425431c01154a17fa14021bde0f0493bd26
John Puddephatt <john@jdp.org.uk>
2021-01-20 21:05:33 +0000

    more activity feed styling

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -12,47 +12,47 @@
               @foreach($activities as $activity)
                 <div class="panel-block is-justify-between">
                   @if(class_basename($activity->subject_type) == 'Comment')
-                      <figure class="image is-64x64 mr-2">
-                          <img class="is-rounded" src="{{$activity->properties['attributes']['user.photo']}}" alt="Image">
-                      </figure>
-                      <div class="media-content">
-                          <p>
-                            {{$activity->properties['attributes']['user.first_name']}} commented on
-                            @if($activity->properties['attributes']['reply.user.id'] == $activity->causer_id)
-                              their own reply:
-                            @else
-                              {{$activity->properties['attributes']['reply.user.first_name']}}’s reply:<p>
-                            @endif
-                          </p>
-                          <div class="is-italic">{!!$activity->properties['attributes']['value'] !!}</div>
-                          <timeago class="is-size-7 has-text-grey" datetime="{{$activity->created_at}}" :auto-update="60"></timeago>
-                      </div>
-                      <a class="button" href="{{ route('lesson.reply', ['course' => $activity->properties['attributes']['reply.lesson.course.id'], 'lesson' => $activity->properties['attributes']['reply.lesson.id'], 'reply_id' => $activity->properties['attributes']['reply.id'], 'show_feedback' => false]) }}">
-                        View
-                      </a>
+                    <figure class="image is-64x64 mr-2 is-align-self-flex-start is-flex-shrink-0">
+                        <img class="is-rounded" src="{{$activity->properties['attributes']['user.photo']}}" alt="Image">
+                    </figure>
+                    <div class="media-content">
+                        <p>
+                          {{$activity->properties['attributes']['user.first_name']}} commented on
+                          @if($activity->properties['attributes']['reply.user.id'] == $activity->causer_id)
+                            their own reply:
+                          @else
+                            {{$activity->properties['attributes']['reply.user.first_name']}}’s reply:<p>
+                          @endif
+                        </p>
+                        <div class="is-italic">{!!$activity->properties['attributes']['value'] !!}</div>
+                        <timeago class="is-size-7 has-text-grey" datetime="{{$activity->created_at}}" :auto-update="60"></timeago>
+                    </div>
+                    <a class="button" href="{{ route('lesson.reply', ['course' => $activity->properties['attributes']['reply.lesson.course.id'], 'lesson' => $activity->properties['attributes']['reply.lesson.id'], 'reply_id' => $activity->properties['attributes']['reply.id'], 'show_feedback' => false]) }}">
+                      View
+                    </a>
                   @endif
 
                   @if(class_basename($activity->subject_type) == 'Reply')
-                      <figure class="image is-64x64 mr-2">
-                          <img class="is-rounded" src="{{$activity->properties['attributes']['user.photo']}}" alt="Image">
-                      </figure>
-                      <div class="media-content">
-                          <p>
-                            @if(isset($activity->properties['attributes']['reply']['user']))
-                              {{$activity->properties['attributes']['user.first_name']}} gave feedback to {{ $activity->properties['attributes']['reply']['user']['first_name'] }} in {{ $activity->properties['attributes']['reply']['lesson']['title'] }}
-                            @else
-                              {{$activity->properties['attributes']['user.first_name']}} posted a reply in {{ $activity->properties['attributes']['lesson.title'] }}
-                            @endif
-                          </p>
-                          <timeago class="is-size-7 has-text-grey" datetime="{{$activity->created_at}}" :auto-update="60"></timeago>
-                      </div>
-                      <a class="button"
-                        @if(isset($activity->properties['attributes']['reply']['user']))
-                          href="{{ route('lesson.reply', ['course' => $activity->properties['attributes']['reply.lesson']['course_id'], 'lesson' => $activity->properties['attributes']['reply.lesson']['id'], 'reply_id' => $activity->properties['attributes']['reply']['id'], 'show_feedback' => isset($activity->properties['attributes']['reply']['user'])]) }}"
-                        @else
-                          href="{{ route('lesson.reply', ['course' => $activity->properties['attributes']['lesson.course.id'], 'lesson' => $activity->properties['attributes']['lesson.id'], 'reply_id' => $activity->subject_id, 'show_feedback' => isset($activity->properties['attributes']['reply']['user'])]) }}"
-                        @endif
-                        >View</a>
+                    <figure class="image is-64x64 mr-2 is-align-self-flex-start is-flex-shrink-0">
+                        <img class="is-rounded" src="{{$activity->properties['attributes']['user.photo']}}" alt="Image">
+                    </figure>
+                    <div class="media-content">
+                        <p>
+                          @if(isset($activity->properties['attributes']['reply']['user']))
+                            {{$activity->properties['attributes']['user.first_name']}} gave feedback to {{ $activity->properties['attributes']['reply']['user']['first_name'] }} in {{ $activity->properties['attributes']['reply']['lesson']['title'] }}
+                          @else
+                            {{$activity->properties['attributes']['user.first_name']}} posted a reply in {{ $activity->properties['attributes']['lesson.title'] }}
+                          @endif
+                        </p>
+                        <timeago class="is-size-7 has-text-grey" datetime="{{$activity->created_at}}" :auto-update="60"></timeago>
+                    </div>
+                    <a class="button"
+                      @if(isset($activity->properties['attributes']['reply']['user']))
+                        href="{{ route('lesson.reply', ['course' => $activity->properties['attributes']['reply.lesson']['course_id'], 'lesson' => $activity->properties['attributes']['reply.lesson']['id'], 'reply_id' => $activity->properties['attributes']['reply']['id'], 'show_feedback' => isset($activity->properties['attributes']['reply']['user'])]) }}"
+                      @else
+                        href="{{ route('lesson.reply', ['course' => $activity->properties['attributes']['lesson.course.id'], 'lesson' => $activity->properties['attributes']['lesson.id'], 'reply_id' => $activity->subject_id, 'show_feedback' => isset($activity->properties['attributes']['reply']['user'])]) }}"
+                      @endif
+                      >View</a>
                   @endif
                 </div>
               @endforeach
